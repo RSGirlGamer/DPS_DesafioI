48a4b46fab5cf1fed6ee9802672074634d819fac
glesi <glesi.mayandy@gmail.com>
2024-08-26 22:28:56 -0600

    Se agrega funcionalidad a pantalla principal, se hace funcionar los filtros con js, se agrega carrito de compras con pantalla modal y calculo de totales.

--- js/script.js
+++ js/script.js
@@ -1,49 +1,136 @@
 import { constantes } from "./constantes.js";
-import {fetchProducts, fetchSingleProduct, fetchCategories, fetcProdByCat} from "./api_connection.js"
-document.addEventListener('DOMContentLoaded', function(){
-     /* fetchProducts(constantes.API_ENDPOINT+constantes.METHODS.GET_ALL);
-     fetchSingleProduct(2);
-     fetchCategories();
-     fetcProdByCat('jewelery'); */
+import { fetchProducts, fetchSingleProduct, fetchCategories, fetcProdByCat } from "./api_connection.js";
 
-     showProducts();
-     showCategories();
-     
-})
+document.addEventListener('DOMContentLoaded', () => {
+    showProducts();
+    showCategories();
+});
 
-async function showProducts(){
-    const productCards = document.querySelector('#container-cards') 
- let productos = await fetchProducts(constantes.API_ENDPOINT+constantes.METHODS.GET_ALL);
+let cart = [];
 
- for (let i = 0; i < productos.length; i++) {
+function addToCart(product) {
+    const existingProduct = cart.find(item => item.id === product.id);
 
-    productCards.innerHTML += `
-    <div class="card mx-2 my-2">
-                            <img class="card-img-top" src="${productos[i].image}" alt="..." />
-                            <div class="card-body p-4">
-                                <div class="text-center">
-                                    <h5 class="fw-bolder">${productos[i].title}</h5>                                    
-                                </div>
-                                <div class="text-center">
-                                $${productos[i].price.toFixed(2)}
-                                </div>
-                            </div>
-                            <!-- Product actions-->
-                            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Comprar</a></div>
-                            </div>
-                        </div>`
-
- }
-console.log(productos);
- }
-
- async function showCategories(){
-    const categoriesSelect = document.querySelector('#categories')
-    let categorias = await fetchCategories();
-    
-    for (let i = 0; i < categorias.length; i++) {
-        categoriesSelect.innerHTML+= `<li><a class="dropdown-item">${categorias[i].charAt(0).toUpperCase()+categorias[i].slice(1)}</a></li>`
-        
+    if (existingProduct) {
+        existingProduct.quantity += 1;
+    } else {
+        cart.push({ ...product, quantity: 1 });
     }
- }
+    updateCartCount();
+}
+
+function updateCartCount() {
+    const cartCount = document.querySelector('.badge');
+    cartCount.textContent = cart.reduce((total, product) => total + product.quantity, 0);
+}
+
+//Se cargan todos los productos y se agrega evento clic
+async function showProducts() {
+    const productCards = document.querySelector('#container-cards');
+    const productos = await fetchProducts(constantes.API_ENDPOINT + constantes.METHODS.GET_ALL);
+
+    productCards.innerHTML = productos.map(producto => `
+        <div class="card mx-2 my-2">
+            <img class="card-img-top" src="${producto.image}" alt="..." />
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <h5 class="fw-bolder">${producto.title}</h5>                                    
+                </div>
+                <div class="text-center">
+                    $${producto.price.toFixed(2)}
+                </div>
+            </div>
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center">
+                    <button class="btn btn-outline-dark mt-auto" onclick="addToCart({id: ${producto.id}, title: '${producto.title}', price: ${producto.price}, image: '${producto.image}'})">
+                        Comprar
+                    </button>
+                </div>
+            </div>
+        </div>`).join('');
+}
+
+async function showCategories() {
+    const categoriesSelect = document.querySelector('#categories');
+    const categorias = await fetchCategories();
+
+    categoriesSelect.innerHTML = categorias.map(categoria => `
+        <li><a class="dropdown-item" href="#" onclick="filterByCategory('${categoria}')">${categoria.charAt(0).toUpperCase() + categoria.slice(1)}</a></li>`
+    ).join('');
+}
+
+async function filterByCategory(category) {
+    const productCards = document.querySelector('#container-cards');
+    const productos = await fetcProdByCat(category);
+
+    productCards.innerHTML = productos.map(producto => `
+        <div class="card mx-2 my-2">
+            <img class="card-img-top" src="${producto.image}" alt="..." />
+            <div class="card-body p-4">
+                <div class="text-center">
+                    <h5 class="fw-bolder">${producto.title}</h5>                                    
+                </div>
+                <div class="text-center">
+                    $${producto.price.toFixed(2)}
+                </div>
+            </div>
+            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                <div class="text-center">
+                    <button class="btn btn-outline-dark mt-auto" onclick="addToCart({id: ${producto.id}, title: '${producto.title}', price: ${producto.price}, image: '${producto.image}'})">
+                        Comprar
+                    </button>
+                </div>
+            </div>
+        </div>`).join('');
+}
+
+function showCart() {
+    const modalBody = document.querySelector('#cart-modal-body');
+    const modal = document.querySelector('#cart-modal');
+    let total = 0;
+
+    modalBody.innerHTML = cart.map(product => {
+        total += product.price * product.quantity;
+        return `
+            <div class="cart-item">
+                <img src="${product.image}" alt="${product.title}" class="cart-item-image">
+                <div class="cart-item-details">
+                    <h5>${product.title}</h5>
+                    <p>Cantidad: ${product.quantity}</p>
+                    <p>Precio: $${product.price.toFixed(2)}</p>
+                </div>
+            </div>`;
+    }).join('');
+
+    modalBody.innerHTML += `
+        <div class="cart-total">
+            <h5>Total: $${total.toFixed(2)}</h5>
+            <button class="btn btn-primary">Pagar</button>
+        </div>`;
+
+    modal.style.display = "block";
+}
+
+// Evento para abrir el modal del carrito
+document.querySelector('.btn-outline-dark').addEventListener('click', (event) => {
+    event.preventDefault();
+    showCart();
+});
+
+// Evnto para cerrar el modal cuando se hace clic en el botón de cerrar
+document.querySelector('.close').addEventListener('click', () => {
+    document.querySelector('#cart-modal').style.display = "none";
+});
+
+// Event listener para cerrar el modal cuando se hace clic fuera del modal
+window.addEventListener('click', (event) => {
+    const modal = document.querySelector('#cart-modal');
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
+
+// Exponer funciones al contexto global
+window.addToCart = addToCart;
+window.showCart = showCart;
+window.filterByCategory = filterByCategory;
